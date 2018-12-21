e7779bbede965199a9585ff93b0d4e975378e5d0
Margrad <marcogranada@gmail.com>
2018-12-21 14:32:13 +0000

    Update column.h

--- GranadaCDB/column.h
+++ GranadaCDB/column.h
@@ -14,6 +14,7 @@ typedef struct _Column_{
     string *name;    // column name
     type tipo;      // type of value in this column
     entry *entry0;  // pointer to the first value in this column
+    entry *last_entry;  // new variable to make it easier the creation of new rows
 } column;
 
 column* NewColumn(string* name,type tipo);
