d8304c532ed5fc17f2929e40df37cac87e580887
Margrad <marcogranada@gmail.com>
2018-12-21 14:34:23 +0000

    Update entry.h

--- GranadaCDB/entry.h
+++ GranadaCDB/entry.h
@@ -10,7 +10,7 @@ typedef enum {TEXT, INTERGER, REAL} type;
 
 typedef struct _timed_entry_{
     time_t change_time;         // time when this entry was created
-    void *next;                 // previows entry from this entry(null will be the first created)
+    struct _timed_entry_ *next;                 // previows entry from this entry(null will be the first created)
     void *value;                // actuall value in this timed_entry
 } timed_entry;
 
@@ -18,7 +18,7 @@ typedef struct _Entry_{
     char lock;
     timed_entry *current_entry; // when edited the DB will save the last entry as a pointer in the latest entry to allow for search in time;
     string *dif_search;    // created from the current_entry to make search without case differentiation
-    void *next_entry;
+    struct _Entry_ *next_entry;
 } entry;
 
 entry* NewEntry(type tipo, void* data);
