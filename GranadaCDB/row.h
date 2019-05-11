206254cbfb78de053b1c5d40fdf9d7ea554564c2
Margrad <marcogranada@gmail.com>
2019-05-11 20:26:59 +0100

    Messing with -gitignore

---
+++ GranadaCDB/row.h
@@ -0,0 +1,11 @@
+
+/**
+this header defines a column for the DB
+**/
+#ifndef _COLUMN
+#define _COLUMN
+#include "entry.h"
+#include <time.h>
+#include "entry.h"
+#include <stdlib.h>
+#include <stdio.h>
