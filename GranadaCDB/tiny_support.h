5e6e93250cd5119c106fc2a69403dded7f723a5a
VATHER\Marco <marcogranada@gmail.com>
2020-12-20 22:37:53 +0000

    formating

--- GranadaCDB/tiny_support.h
+++ GranadaCDB/tiny_support.h
@@ -10,31 +10,32 @@ union _t2_{
 };
 
 
-string* convert_time_to_String(time_t value){
-string * time_to_string = (string *)malloc(sizeof(string));
-int i;
-if (sizeof(time_t)== 8){
-    union _t1_ time;
-    time.time = value;
-    time.text[8] = '\0';
-    time_to_string->len=9;
-    time_to_string->text = (char *)malloc(sizeof(char)*9);
-    for(i=0;i<time_to_string->len;i++){
-        time_to_string->text[i]=time.text[i];
-    }
+string* convert_time_to_String(time_t value)
+{
+    string * time_to_string = (string *)malloc(sizeof(string));
+    int i;
+    if (sizeof(time_t)== 8){
+        union _t1_ time;
+        time.time = value;
+        time.text[8] = '\0';
+        time_to_string->len=9;
+        time_to_string->text = (char *)malloc(sizeof(char)*9);
+        for(i=0;i<time_to_string->len;i++){
+            time_to_string->text[i]=time.text[i];
+        }
 
-}else if(sizeof(time_t)== 4){
-    union _t1_ time;
-    time.time = value;
-    time.text[5] = '\0';
-    time_to_string->len=5;
-    time_to_string->text = (char *)malloc(sizeof(char)*5);
-    for(i=0;i<time_to_string->len;i++){
-        time_to_string->text[i]=time.text[i];
-    }
+    }else if(sizeof(time_t)== 4){
+        union _t1_ time;
+        time.time = value;
+        time.text[5] = '\0';
+        time_to_string->len=5;
+        time_to_string->text = (char *)malloc(sizeof(char)*5);
+        for(i=0;i<time_to_string->len;i++){
+            time_to_string->text[i]=time.text[i];
+        }
 
-}else{
-    printf("Error: time_t has unusual size");
+    }else{
+        printf("Error: time_t has unusual size");
 }
 
 
